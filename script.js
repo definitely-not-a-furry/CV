ea4d1bcbbd97350f37acba1ce1031d858cfe9dc3
definitely-not-a-furry <heijnisties@outlook.com>
2024-03-21 15:13:26 +0100

    Solved bugs

--- script.js
+++ script.js
@@ -32,17 +32,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" slideactive", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " slideactive";
 } 
 
 document.onload = (() => {showSlides(1);})
